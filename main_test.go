56fee224b1e4c95b199591932d9e81126655fd89
k1LoW <k1lowxb@gmail.com>
2022-08-26 11:20:02 +0900

    Fix test

--- main_test.go
+++ main_test.go
@@ -5,29 +5,32 @@ import (
 )
 
 func TestFizBuz(t *testing.T) {
-	tests := map[string]struct {
+	tests := []struct {
+		name    string // テスト内容
 		in      string // 入力
 		want    string // 期待する返り値
 		wantErr bool   // エラーになることを期待するかどうか
 	}{
-		"3の倍数ならFizz":  {"3", "Fizz", false},
-		"5の倍数ならBuzz":  {"5", "Buzz", false},
-		"15の倍数ならBuzz": {"15", "FizzBuzz", false},
+		{"3の倍数ならFizz", "3", "Fizz", false},
+		{"5の倍数ならBuzz", "5", "Buzz", false},
+		{"15の倍数ならBuzz", "15", "FizzBuzz", false},
 	}
 	for _, tt := range tests {
-		got, err := FizBuz(tt.in)
-		if err != nil {
-			if !tt.wantErr {
-				t.Error(err)
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FizBuz(tt.in)
+			if err != nil {
+				if !tt.wantErr {
+					t.Error(err)
+				}
+				return
 			}
-			continue
-		}
-		if tt.wantErr {
-			t.Error("should not be error")
-			continue
-		}
-		if got != tt.want {
-			t.Errorf("got %v\nwant %v", got, tt.want)
-		}
+			if tt.wantErr {
+				t.Error("should not be error")
+				return
+			}
+			if got != tt.want {
+				t.Errorf("got %v\nwant %v", got, tt.want)
+			}
+		})
 	}
 }
